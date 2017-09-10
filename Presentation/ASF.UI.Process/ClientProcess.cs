56aec8f8cfbe9a60b070bf49523755d74ddff43b
Matt <Matt@DESKTOP-GQ73ITO>
2017-09-10 17:11:46 -0300

    Se agregan los Process que faltaban.

---
+++ Presentation/ASF.UI.Process/ClientProcess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using ASF.Entities;
+using ASF.Services.Contracts;
+using ASF.UI.Process;
+
+namespace ASF.UI.Process
+{
+    public class ClientProcess : ProcessComponent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Client> SelectList()
+        {
+            var response = HttpGet<AllResponse>("rest/Client/All", new Dictionary<string, object>(), MediaType.Json);
+            return response.ResultClient;
+        }
+    }
+}
---
+++ Presentation/ASF.UI.Process/ProductProcess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using ASF.Entities;
+using ASF.Services.Contracts;
+using ASF.UI.Process;
+
+namespace ASF.UI.Process
+{
+    public class ProductProcess : ProcessComponent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Product> SelectList()
+        {
+            var response = HttpGet<AllResponse>("rest/Product/All", new Dictionary<string, object>(), MediaType.Json);
+            return response.ResultProduct;
+        }
+    }
+}
