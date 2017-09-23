d3cea97a036a71489a65fadbed8239b898aa35ea
Matt <Matt@DESKTOP-GQ73ITO>
2017-09-23 19:28:44 -0300

    Se agregan todas las Areas y el ABM de CartItem

---
+++ Presentation/ASF.UI.WbSite/Areas/CartItems/Controllers/CartItemController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASF.Entities;
+using ASF.UI.Process;
+
+namespace ASF.UI.WbSite.Areas.CartItems.Controllers
+{
+    public class CartItemController : Controller
+    {
+        // GET: CartItems/CartItem
+        public ActionResult Index()
+        {
+            CartItemProcess cartitemprocess = new CartItemProcess();
+            var lista = cartitemprocess.SelectList();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CartItem cartitem)
+        {
+            var cp = new CartItemProcess();
+            cp.Add(cartitem);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var cp = new CartItemProcess();
+            var cartitem = cp.FindById(id);
+            return View(cartitem);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CartItem cartitem)
+        {
+            var cp = new CartItemProcess();
+            cp.Edit(cartitem);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var cp = new CartItemProcess();
+            var cartitem = cp.FindById(id);
+            return View(cartitem);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(CartItem cartitem)
+        {
+            var cp = new CartItemProcess();
+            cp.Delete(cartitem);
+            return RedirectToAction("Index");
+        }
+    }
+}
